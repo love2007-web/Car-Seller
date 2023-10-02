42199e033e8921b22b5c32189fa642e4354b14a2
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-10-02 18:14:39 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='shadow-lg bg-slate-200 flex items-center h-16 p-3'>
+            <input type="text" placeholder='Search...' className='w-60 p-2 rounded-lg' />
+        </div>
+    </>
+  )
+}
+
+export default Navbar
